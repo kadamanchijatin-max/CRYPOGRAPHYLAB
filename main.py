0ea528d142148f55de4b49d30413d4dd0550c6cb
kadamanchijatin-x <kadamanchijatin@gmail.com>
2026-07-29 12:21:21 +0530

    added the menu driven command line

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("ok")
+def menu():
+    while True:
+        print("\n===== CryptoLabX =====")
+        print("1. Encrypt")
+        print("2. Decrypt")
+        print("3. Attack")
+        print("4. Analyze")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            print("Encrypt - Coming Soon")
+
+        elif choice == "2":
+            print("Decrypt - Coming Soon")
+
+        elif choice == "3":
+            print("Attack - Coming Soon")
+
+        elif choice == "4":
+            print("Analyze - Coming Soon")
+
+        elif choice == "5":
+            print("Exiting CryptoLabX...")
+            break
+
+        else:
+            print("Invalid choice. Try again.")
+
+
+if __name__ == "__main__":
+    menu()
